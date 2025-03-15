98933fe3bbe78f410752a0e4b57c3f7f069ca00a
junker530 <ddr4646@gmail.com>
2025-03-15 15:54:58 +0900

    Initial Commit

---
+++ 3DCard/_DCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardApp.swift
+//  3DCard
+//
+//  Created by Shota Sakoda on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct _DCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3DCardTests/_DCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardTests.swift
+//  3DCardTests
+//
+//  Created by Shota Sakoda on 2025/03/15.
+//
+
+import Testing
+@testable import _DCard
+
+struct _DCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
